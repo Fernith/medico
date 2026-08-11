9f4485d72adee85e4324de256c22ecfc5912405c
Fernith <fernando.tena.hc@gmail.com>
2026-08-11 21:06:46 +0200

    Cambios main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -13,15 +13,28 @@ mod tasks;
 async fn main() {
     let configuracion = config::AppConfig::init().await;
 
-    // 2. Iniciamos el cron y el disparador delegándolo al nuevo archivo
+    // Iniciamos las tareas de fondo
     tasks::iniciar_tareas_de_fondo(configuracion.database_pool.clone()).await;
 
     // Enrutador Principal de la API
     let api_router = routes::construir_router(configuracion.database_pool);
     
-    // Configurar el SPA: Sirve la carpeta dist, y si no encuentra la ruta, devuelve index.html
-    let frontend_service = ServeDir::new("frontend/dist")
-        .not_found_service(ServeFile::new("frontend/dist/index.html"));
+    // --- BUSCADOR DE RUTAS A PRUEBA DE BALAS ---
+    let mut ruta_estaticos = "frontend/dist"; // Ruta por defecto
+    
+    if std::path::Path::new("/app/frontend/dist").exists() {
+        ruta_estaticos = "/app/frontend/dist"; // 1. Entorno de Producción (Docker en Render)
+    } else if std::path::Path::new("../frontend/dist").exists() {
+        ruta_estaticos = "../frontend/dist"; // 2. Entorno Local (ejecutando desde /backend)
+    } else if std::path::Path::new("dist").exists() {
+        ruta_estaticos = "dist"; // 3. Entorno Local Alternativo
+    }
+
+    println!("📂 Sirviendo el frontend desde: {}", ruta_estaticos);
+
+    // Configurar el SPA: Sirve la carpeta detectada, y si no encuentra la ruta, devuelve index.html
+    let frontend_service = ServeDir::new(ruta_estaticos)
+        .not_found_service(ServeFile::new(format!("{}/index.html", ruta_estaticos)));
 
     let app = Router::new()
         .merge(api_router)
@@ -32,7 +45,6 @@ async fn main() {
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
-    // Encendido del Servidor
     println!("🚀 Servidor Full Stack corriendo en: http://{}", addr);
     
     let oyente = tokio::net::TcpListener::bind(&addr).await.unwrap();
